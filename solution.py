d6b73c16af031cce4631e6988e3eb6267b98748e
César Figueroa <cesarabelfigueroa@gmail.com>
2023-11-26 14:53:06 -0600

    add value method

--- solution.py
+++ solution.py
@@ -3,3 +3,14 @@ class DataCapture:
         self.data = []
         self.count_less = {}
         self.count_greater = {}
+    
+    def add(self, value):
+        self.data.append(value)
+        
+        if value not in self.count_less:
+            self.count_less[value] = 0
+        self.count_less[value] += 1
+
+        if value not in self.count_greater:
+            self.count_greater[value] = 0
+        self.count_greater[value] += 1
