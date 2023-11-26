86cbcf671a4863a601369b551b00c7a3e408480c
César Figueroa <cesarabelfigueroa@gmail.com>
2023-11-26 15:11:35 -0600

    add testing and solving issues

--- solution.py
+++ solution.py
@@ -1,33 +1,38 @@
 class DataCapture:
     def __init__(self):
-        self.data = []
-        self.count_less = {}
-        self.count_greater = {}
-    
+        self.data = [0] * 1000  # Assuming all values are less than 1000
+
     def add(self, value):
-        self.data.append(value)
+        if 0 <= value < 1000:
+            self.data[value] += 1
 
-        if value not in self.count_less:
-            self.count_less[value] = 0
-        self.count_less[value] += 1
+    def build_stats(self):
+        total_count = 0
+        self.less_than = [0] * 1000
+        self.greater_than = [0] * 1000
 
-        if value not in self.count_greater:
-            self.count_greater[value] = 0
-        self.count_greater[value] += 1
-    
+        for i in range(1000):
+            self.less_than[i] = total_count
+            total_count += self.data[i]
+
+        total_count = 0
+        for i in reversed(range(1000)):
+            self.greater_than[i] = total_count
+            total_count += self.data[i]
+
+        return self
+
     def less(self, value):
-        if value not in self.count_less:
-            return 0
-        return self.count_less[value]
+        if 0 <= value < 1000:
+            return self.less_than[value]
+        return 0
 
     def greater(self, value):
-        if value not in self.count_greater:
-            return 0
-        return self.count_greater[value]
+        if 0 <= value < 1000:
+            return self.greater_than[value]
+        return 0
 
-    def between(self, lower, upper):
-        count = 0
-        for value in self.data:
-            if lower <= value <= upper:
-                count += 1
-        return count
+    def between(self, start, end):
+        if 0 <= start <= end < 1000:
+            return self.less_than[end + 1] - self.less_than[start]
+        return 0
---
+++ test_data_capture.py
@@ -0,0 +1,32 @@
+from solution import DataCapture
+
+class TestDataCapture:
+    def test_less(self):
+        capture = DataCapture()
+        capture.add(3)
+        capture.add(9)
+        capture.add(3)
+        capture.add(4)
+        capture.add(6)
+        stats = capture.build_stats()
+        assert stats.less(4) == 2
+
+    def test_greater(self):
+        capture = DataCapture()
+        capture.add(3)
+        capture.add(9)
+        capture.add(3)
+        capture.add(4)
+        capture.add(6)
+        stats = capture.build_stats()
+        assert stats.greater(4) == 2
+
+    def test_between(self):
+        capture = DataCapture()
+        capture.add(3)
+        capture.add(9)
+        capture.add(3)
+        capture.add(4)
+        capture.add(6)
+        stats = capture.build_stats()
+        assert stats.between(3, 6) == 4
